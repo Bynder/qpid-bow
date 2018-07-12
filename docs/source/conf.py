ef7b0125c195e207580abf7ce6a8fce957c25e6e
Matyas Bela Kuti <matyas@bynder.com>
2018-07-12 13:25:07 +0000

    Bump version to 1.0.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ copyright = '2018, Bynder B.V.'
 author = 'Bynder B.V.'
 
 version = '1.0'  # The short X.Y version.
-release = '1.0.0'  # The full version, including alpha/beta/rc tags.
+release = '1.0.2'  # The full version, including alpha/beta/rc tags.
 
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
