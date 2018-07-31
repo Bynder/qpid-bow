2810c38ad3351552edd58631e9b87818fd2248ce
Michel Bouwmans <michel@bynder.com>
2018-07-31 09:13:31 +0000

    Add support for amqp+ssl URL's and configured username and password

--- qpid_bow/config.py
+++ qpid_bow/config.py
@@ -7,6 +7,8 @@ from typing import (
     Optional,
 )
 
+from urllib.parse import urlsplit, urlunsplit
+
 config: dict = {}
 
 
@@ -19,30 +21,50 @@ def configure(new_config: Mapping):
     config.update(new_config)
 
 
-def get_urls(urls: Optional[str] = None) -> List[str]:
-    """Retrieves server urls from one of the sources.
+def process_url(url: str) -> str:
+    """Processes a URL for usage with Qpid Proton.
+
+    - ActiveMQ amqp+ssl scheme is replaced with amqps.
+    - Adds username and password from config.
+
+    Args:
+        url: Input URL.
+
+    Returns:
+        str: Processed URL.
+    """
+    splited_url = urlsplit(url.strip())
+    if splited_url.scheme == 'amqp+ssl':
+        splited_url = splited_url._replace(scheme='amqps')
+
+    if ((not splited_url.username or not splited_url.password) and
+            'username' in config and 'password' in config):
+        user_pass = f"{config['username']}:{config['password']}@"
+        new_netloc = user_pass + splited_url.netloc
+        splited_url = splited_url._replace(netloc=new_netloc)
+
+    return urlunsplit(splited_url)
+
+
+def get_urls(argument_urls: Optional[str] = None) -> List[str]:
+    """Retrieves server argument_urls from one of the sources.
 
     The sources priority comes in the following order: passed arguments,
     global config, AMQP_SERVERS environment variable.
 
     Args:
-        urls: Comma-separated urls.
+        argument_urls: Comma-separated argument_urls.
 
     Returns:
-        List[str]: Returns list of urls to connect to.
+        List[str]: Returns list of argument_urls to connect to.
     """
-    if urls:
-        return [url.strip() for url in urls.split(',')]
+    if argument_urls:
+        raw_urls = argument_urls
+    elif 'amqp_url' in config:
+        raw_urls = config['amqp_url']
+    elif 'AMQP_SERVERS' in environ:
+        raw_urls = environ['AMQP_SERVERS']
+    else:
+        raise ValueError('AMQP server url is not configured')
 
-    if config.get('amqp_url'):
-        return [url.strip() for url in config['amqp_url'].split(',')]
-
-    amqp_servers = environ.get('AMQP_SERVERS')
-
-    if amqp_servers:
-        environ_urls = []
-        for server in amqp_servers.split(','):
-            environ_urls.append(server.strip())
-        return environ_urls
-
-    raise ValueError('AMQP server url is not configured')
+    return [process_url(url) for url in raw_urls.split(',')]
